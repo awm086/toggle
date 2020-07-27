ea48dd892d52e76b0c39d58726553df67c237229
Ali <aalmusta@its.jnj.com>
2020-07-27 15:52:35 -0400

    working api

---
+++ src/InvoiceGenerator.php
@@ -0,0 +1,115 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Reader\Xls;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Writer;
+use Toggle\ToggleHTTPClient;
+use Toggle\InvoiceClient;
+
+class InvoiceGenerator
+{
+
+    /**
+     * @var InvoiceClient
+     */
+    public $invoiceClient;
+
+    /**
+     * Path to the template to use
+     * @var string
+     */
+    public $templatePath;
+
+    /**
+     * Rate
+     * @var int
+     */
+    public $rate;
+
+
+    /**
+     * Path to the template to use
+     * @var Worksheet
+     */
+    private $worksheet;
+
+    /**
+     *
+     * @var Writer\IWriter
+     */
+    private $writer;
+
+    public function __construct(InvoiceClient $client = NULL,  $template = '/invoice_templates/template.xlsx', $rate = 75)
+    {
+        $this->invoiceClient = $client;
+        $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load(__DIR__ . $template);
+        $this->worksheet = $spreadsheet->getActiveSheet();
+        $this->writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $this->rate = $rate;
+    }
+
+    public function genInvoice($info, $outPutFileName = 'Invoice Service Invoice Hourly Rate.xlsx')
+    {
+
+        $data = $this->invoiceClient->getData($info);
+
+
+        $template_row = 15;
+        $worksheet = $this->worksheet;
+        $worksheet->insertNewRowBefore(16, count($data) - 1);
+
+        if (!empty($data)) {
+            $total = 0;
+            $total_hours = 0;
+            foreach ($data as $row_index => $row_value) {
+                $decimalHrs =  $this->toDecimal($this->formatMilliseconds($row_value[2]));
+                $worksheet->setCellValue('B' . $template_row, $row_value[0]);
+                $worksheet->setCellValue('C' . $template_row,  date('Y-m-d', strtotime($row_value[1])));
+                $worksheet->setCellValue('D' . $template_row, $decimalHrs);
+                $total_hours = $total_hours + $decimalHrs;
+                $worksheet->setCellValue('E' . $template_row, $this->rate);
+                $row_total = $this->rate * $decimalHrs;
+                $worksheet->setCellValue('F' . $template_row, $row_total);
+                $total = $total + $row_total;
+                $template_row++;
+            }
+
+            $worksheet->setCellValue('F' . $template_row, $total);
+            $worksheet->setCellValue('D' . $template_row, 'HOURS: ' . $total_hours);
+        }
+        $this->writer->save($outPutFileName);
+    }
+
+    function formatMilliseconds($milliseconds) {
+        $seconds = floor($milliseconds / 1000);
+        $minutes = floor($seconds / 60);
+        $hours = floor($minutes / 60);
+        $milliseconds = $milliseconds % 1000;
+        $seconds = $seconds % 60;
+        $minutes = $minutes % 60;
+        $format = '%u:%02u:%02u.%03u';
+        $time = sprintf($format, $hours, $minutes, $seconds, $milliseconds);
+        return rtrim($time, '0');
+    }
+
+    /**
+     * Convert time into decimal time.
+     *
+     * @param string $time The time to convert
+     *
+     * @return integer The time as a decimal value.
+     */
+    function toDecimal($time)
+    {
+        $timeArr = explode(':', $time);
+        $decTime = ($timeArr[0] * 60) + ($timeArr[1]) + ($timeArr[2] / 60);
+        return round($decTime / 60,3);
+    }
+}
+ $myClient = new ToggleHTTPClient('4ce66fd47803a241ec4f2c4a510f322a', '950575');
+ $invoiceGenerator = new InvoiceGenerator($myClient);
+ $invoiceGenerator->genInvoice(['since'=> '2020-07-13', 'until' => '2020-07-26', 'user' => '4431821']);
+echo "hello";
---
+++ src/Toggle/ToggleHTTPClient.php
@@ -0,0 +1,93 @@
+<?php
+namespace Toggle;
+
+
+use GuzzleHttp\Client as Guzzle;
+
+
+interface InvoiceClient
+{
+    public function getData($info = NULL);
+}
+
+class ToggleHTTPClient extends Guzzle implements InvoiceClient
+{
+    const BASE_URI = 'https://toggl.com';
+    private $api_token;
+    private $workspace_id;
+    private $config;
+
+    public function __construct($api_token, $workspace_id)
+    {
+        $config = [
+            'base_uri' => self::BASE_URI,
+            'query' => ['workspace_id' => $workspace_id, 'user_agent' => 'api_test'],
+            'auth' => [$api_token, 'api_token']
+        ];
+        $this->config = $config;
+        parent::__construct($config);
+    }
+
+    public function getDetailedReport($since, $until, $user = NULL)
+    {
+        $resource =  '/reports/api/v2/details';
+        $reportOptions = ['since' => $since, 'until' => $until];
+        if ($user) {
+            $reportOptions['user_ids'] = $user;
+        }
+        $this->config['query'] = array_merge($this->config['query'], $reportOptions);
+
+        return json_decode(
+            (string) parent::get($resource, $this->config)->getBody()
+        );
+    }
+
+    /**
+     * Get the data as we expect it to fill the timesheet template.
+     *
+     * @param [type] $info
+     * @return array
+     *   return an array of data that has desc, data, and duration
+     */
+    public function getData($info = NULL)
+    {
+        if (isset($info['since']) && isset($info['until'])) {
+            $user = isset($info['user']) ?  $info['user'] : NULL;
+            $report = $this->getDetailedReport($info['since'], $info['until'], $user);
+            $ret = [];
+            foreach ($report->data as $key => $value) {
+                $ret[$key] = [
+                    $value->description,
+                    $value->start,
+                    $value->dur
+                ];
+            }
+            return $ret;
+        }
+    }
+}
+
+//  curl -v -u 4ce66fd47803a241ec4f2c4a510f322a:api_token -X GET "https://toggl.com/reports/api/v2/details\?workspace_id=950575&since=2020-05-15&until=2020-07-14&user_agent\=api_test"
+// $options = [
+//     'base_uri' => 'https://toggl.com',
+//     'auth' => ['4ce66fd47803a241ec4f2c4a510f322a', 'api_token'],
+//     'query' => [
+//         'user_agent' => 'api_test',
+//         'workspace_id' => '950575',
+//         'since' => '2020-06-15',
+//         'until' => '2020-07-12',
+//         'user_ids' => '4431821',
+//     ],
+// ];
+// $client = new GuzzleHttp\Client();
+// $response = $client->get('/reports/api/v2/details',  $options);
+
+
+$myClient = new ToggleHTTPClient('4ce66fd47803a241ec4f2c4a510f322a', '950575');
+$report = $myClient->getDetailedReport('2020-06-15', '2020-07-12', '4431821');
+$data = $myClient->getData(['since' => '2020-06-15', 'until' => '2020-07-12', 'user' => '4431821' ]);
+print_r($data);
+
+// echo $response->getStatusCode();
+// $json = json_decode($response->getBody());
+// print_r($json);
